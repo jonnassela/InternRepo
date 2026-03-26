cd5731a1be2f3d41c29908fb41dc214bfe8f8861
Jona <js30873@seeu.edu.mk>
2026-03-26 19:52:51 +0100

    Enhanced Full Load with TRY-CATCH and audit logging

--- full_load.sql
+++ full_load.sql
@@ -1,16 +1,44 @@
 -- full load logic// Delete everything → Insert everything again
 -- Full load replaces all data in the target table with fresh data from the source to ensure consistency.
 
-create procedure full_load_customers
-as 
-begin
+-- create procedure full_load_customers
+-- as 
+-- begin
 
-    --remove old data
-    delete from staging.customers_clean;
+--     --remove old data
+--     delete from staging.customers_clean;
 
-    --load fresh data
-    insert into staging.customers_clean
-    select *
-    from landing.customers;
+--     --load fresh data
+--     insert into staging.customers_clean
+--     select *
+--     from landing.customers;
 
-end;
+-- end;
+
+CREATE PROCEDURE full_load_customers
+AS
+BEGIN
+
+    BEGIN TRY
+
+        DELETE FROM staging.customers_clean;
+
+        INSERT INTO staging.customers_clean
+        SELECT *
+        FROM landing.customers;
+
+        INSERT INTO config.audit_log
+        (procedure_name, status, message)
+        VALUES ('full_load_customers', 'SUCCESS', 'Full load completed');
+
+    END TRY
+
+    BEGIN CATCH
+
+        INSERT INTO config.audit_log
+        (procedure_name, status, message)
+        VALUES ('full_load_customers', 'ERROR', ERROR_MESSAGE());
+
+    END CATCH
+
+END;
