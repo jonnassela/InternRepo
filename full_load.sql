822346c02fe96341e8e4bfbce384f7f0adffdd39
Jona <js30873@seeu.edu.mk>
2026-03-26 19:41:55 +0100

    Added basuc full load procedure (delete and insert logic)

--- full_load.sql
+++ full_load.sql
@@ -1,1 +1,16 @@
--- full load logic
+-- full load logic// Delete everything → Insert everything again
+-- Full load replaces all data in the target table with fresh data from the source to ensure consistency.
+
+create procedure full_load_customers
+as 
+begin
+
+    --remove old data
+    delete from staging.customers_clean;
+
+    --load fresh data
+    insert into staging.customers_clean
+    select *
+    from landing.customers;
+
+end;
