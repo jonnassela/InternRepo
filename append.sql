66b6eae9dc9852d08a17c2ac7c0c60662fb9b34d
Jona <js30873@seeu.edu.mk>
2026-03-28 19:52:27 +0100

    Implemented basic append logic (insert all records from landing)

--- append.sql
+++ append.sql
@@ -1,1 +1,12 @@
 --append logic
+--“Insert new data into the target table without modifying existing data.”
+create or alter procedure append_customers
+
+as 
+begin 
+
+    insert into staging.customers_clean(customer_id,name,city,updated)
+    select customer_id,name,city,updated
+    from landing.customers;
+
+end;
